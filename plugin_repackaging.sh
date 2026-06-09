8aa9658f3ef4a8e7ecda8173464b36828efb41c6
Caiyi <39944114+matdwcy@users.noreply.github.com>
2026-06-09 11:23:18 +0800

    Update plugin_repackaging.sh

--- plugin_repackaging.sh
+++ plugin_repackaging.sh
@@ -333,7 +333,9 @@ PY
 
 	mkdir -p ./wheels
 	echo "Downloading wheels to ./wheels/..."
-	sed -i 's/gevent==26\.5\.0/gevent==25.9.1/g' requirements.txt
+	sed -i 's/gevent==25\.5\.1/gevent==24.11.1/g' requirements.txt
+	sed -i 's/gevent==26\.5\.0/gevent==24.11.1/g' requirements.txt
+	sed -i 's/greenlet==3\.4\.0/greenlet==3.1.1/g' requirements.txt
 	${PIP_CMD} download ${PIP_PLATFORM} --prefer-binary -r requirements.txt -d ./wheels \
 		--index-url ${PIP_MIRROR_URL} --trusted-host mirrors.aliyun.com
 	if [[ $? -ne 0 ]]; then
